12a97024b7a2139e0a80212b640db0f3ff289420
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-22 01:14:50 +0300

    Python Functions

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,20 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+    return num1 + num2
 
 def halve(number):
     pass
+    return number / 2
